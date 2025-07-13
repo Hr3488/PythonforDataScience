12cd563aabff75c49c3007b0778befe530c18360
ramsuncheek <ram3488@gmail.com>
2025-07-13 22:18:36 +0530

    Lastest

--- first.py
+++ first.py
@@ -2,9 +2,18 @@
 # b=a*20
 # print(a,b)
 
-Employee_name="RAMKUMAR"
-Age=38
-Height=5.2
-print(type(Employee_name))
-print(type(Age))
-print(type(Height))
+# Employee_name="RAMKUMAR"
+# Age=38
+# Height=5.2
+# print(type(Employee_name))
+# print(type(Age))
+# print(type(Height))
+
+# Operators
+# a=10
+# b=5
+# c=a+b
+# print(c)
+
+A=7-2*(27/3**2)+4
+print(A)
