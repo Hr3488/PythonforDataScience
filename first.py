534403734054679661a6becbb8fe84db13b981a9
ramsuncheek <ram3488@gmail.com>
2025-07-14 07:24:11 +0530

    Last change with Squences

--- first.py
+++ first.py
@@ -15,5 +15,18 @@
 # c=a+b
 # print(c)
 
-A=7-2*(27/3**2)+4
-print(A)
+# A=7-2*(27/3**2)+4
+# print(A)
+
+# Sequences 
+
+strString="Learnings","Ram"
+list=[1,2,3,4,5,6,5,6],[2,2,2,3,3,4]
+print(strString)
+print(list)
+List_a=[1,'ram',"Learning",'a']
+print(List_a)
+array_Sample =('a',[2,3,4,5])
+print(array_Sample)
+b=[2,3,4,5]
+print(b)
